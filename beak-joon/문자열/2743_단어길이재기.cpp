0f4785eac4a3985c2c2da0ba1067084bc2cf6290
jinhea <nordstromm03@gmail.com>
2023-05-10 23:40:10 +0900

    문자열 문제 풀이 추가

---
+++ beak-joon/문자열/2743_단어길이재기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+  
+  char str[101];
+
+  cin >> str;
+  cout << strlen(str) << '\n';
+
+}
+
+/*
+    #include <iostream>
+    #include <string.h>
+
+    using namespace std;
+
+    int main()
+    {
+    
+        char str[101];
+
+        cin >> str;
+        cout << strlen(str) << '\n';
+
+    }
+*/
