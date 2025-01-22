4c2dcd615b5d1a84a78cb71b75a94af35061b116
yeon-so <nordstromm03@gmail.com>
2025-01-22 17:06:15 +0900

    feat: 자료구조 문제 추가 (silver 4 - 1620: 나는야 포켓몬 마스터 이다솜, 100692kb, 1112ms, java11)

---
+++ beakjoon/정렬/JAVA/Exam_1620.java
@@ -0,0 +1,49 @@
+package beakjoon.정렬.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Exam_1620 {
+
+    static boolean isNumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        HashMap<Integer, String> map = new HashMap<>();
+        HashMap<String, Integer> reversMap = new HashMap<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= n; i++) {
+            String name = br.readLine();
+            map.put(i, name);
+            reversMap.put(name, i);
+        }
+
+        for(int i = 0; i < m; i++) {
+            String str = br.readLine();
+            
+            if(isNumber(str)){
+                int num = Integer.parseInt(str);
+                System.out.println(map.get(num));
+            } else {
+                System.out.println(reversMap.get(str));
+            }
+
+        }
+
+    }
+}
