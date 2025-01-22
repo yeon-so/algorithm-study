d6af71f6743abb423fef5bbea6e3db4182115ef2
yeon-so <nordstromm03@gmail.com>
2025-01-22 17:05:28 +0900

    feat: 자료구조 문제 추가 (silver 5 - 7785: 회사에 있는 사람, 55088kb, 1196ms, java11)

---
+++ beakjoon/자료구조/JAVA/Exam_7785.java
@@ -0,0 +1,41 @@
+package beakjoon.정렬.JAVA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Exam_7785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int n = Integer.parseInt(br.readLine());
+        HashMap<String, String> map = new HashMap<>();
+        
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String k = st.nextToken();
+            String v = st.nextToken();
+
+            if(v.equals("enter")) {
+                map.put(k, v);
+            } else {
+                map.remove(k);
+            }
+        }
+
+        br.close();
+
+        List<String> keList = new ArrayList<>(map.keySet());
+        Collections.sort(keList, Collections.reverseOrder());
+
+        for(String e: keList) {
+            System.out.println(e);
+        }
+    }
+}
