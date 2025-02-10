7a8bca03fa0b3785c8f4a755e205505ec35aa573
yeon-so <nordstromm03@gmail.com>
2025-02-10 20:05:18 +0900

    leve1 2 - 전화번호 목록

--- programmers/해시/전화번호_목록.java
+++ programmers/해시/전화번호_목록.java
@@ -1,5 +1,23 @@
 package programmers.해시;
 
+import java.util.Arrays;
+
 public class 전화번호_목록 {
+    /*
+    * array
+    */
+    public boolean solution1(String[] phone_book) {
+        boolean answer = true;
+        
+        Arrays.sort(phone_book);
+        
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
     
+        return answer;
+    }
 }
