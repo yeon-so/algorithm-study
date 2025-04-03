116a78aa52342e71242627897c76333316597729
yeon-so <nordstromm03@gmail.com>
2025-04-03 12:46:13 +0900

    Easy - TapeEquilibrium

---
+++ codility/TapeEquilibrium.java
@@ -0,0 +1,26 @@
+package codility;
+
+public class TapeEquilibrium {
+    class Solution {
+        public int solution(int[] A) {
+            int a = 0;
+            int b = 0;
+            int answer = Integer.MAX_VALUE;
+    
+            for(int i = 0; i < A.length; i++) {
+                b += A[i];
+            }
+    
+            for(int i = 0; i < A.length - 1; i++) {
+                a += A[i];
+                b -= A[i];
+    
+                int temp = Math.abs(a - b);
+    
+                answer = Math.min(answer, temp);
+            }
+    
+            return answer;
+        }
+    }
+}
