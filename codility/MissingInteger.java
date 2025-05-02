74c4e3dc2632bffbec49b8532bef8f3e1b688a47
yeon-so <nordstromm03@gmail.com>
2025-05-02 21:01:04 +0900

    Medium - MissingInteger

---
+++ codility/MissingInteger.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingInteger {
+
+    // 1. 정렬 사용 - O(NlogN)
+    public int solution1(int[] A) {
+            int min = 1;
+
+            // O(NlogN)
+            Arrays.sort(A);
+
+            for(int i = 0; i < A.length; i++) {
+                if(A[i] == min) {
+                    min++;
+                    continue;
+                }
+
+                if(A[i] > min) {
+                    break;
+                }
+            }
+
+            return min;
+        }
+
+    // 2. Set 사용 - O(N)
+    public int solution2(int[] A) {
+            int min = 1;
+
+            if(A.length == 0) {
+                return 1;
+            }
+
+            Set<Integer> set = new HashSet<>();
+
+            for(int i = 0; i < A.length; i++) {
+                set.add(A[i]);
+            }
+
+            return min;
+    }
+
+}
