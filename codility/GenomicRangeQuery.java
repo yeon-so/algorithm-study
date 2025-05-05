b8e9ed1e00e322a23e422d9ca89eed41fba17148
yeon-so <nordstromm03@gmail.com>
2025-05-05 21:57:09 +0900

    Medium - GenomicRangeQuery

--- codility/GenomicRangeQuery.java
+++ codility/GenomicRangeQuery.java
@@ -37,4 +37,43 @@ public class GenomicRangeQuery {
         return result;
 
     }
+
+    // O(N + M) - 누적합 사용
+    public int[] solution2(String S, int[] P, int[] Q) {
+        int[][] dnaPrefixNum = new int[3][S.length()+1]; 
+        int[] result = new int[P.length];
+
+        for(int i = 0; i < S.length(); i++) {
+            char tempChar = S.charAt(i);
+
+            for (int j = 0; j < 3; j++) {
+                dnaPrefixNum[j][i + 1] = dnaPrefixNum[j][i]; 
+            }
+
+            if (tempChar == 'A') {
+                dnaPrefixNum[0][i + 1]++;
+            } else if (tempChar == 'C') {
+                dnaPrefixNum[1][i + 1]++;
+            } else if (tempChar == 'G') {
+                dnaPrefixNum[2][i + 1]++;
+            }
+        }
+
+        for(int i = 0; i < P.length; i++) {
+            int from = P[i];
+            int to = Q[i] + 1;
+
+            if (dnaPrefixNum[0][to] - dnaPrefixNum[0][from] > 0)
+                result[i] = 1;
+            else if (dnaPrefixNum[1][to] - dnaPrefixNum[1][from] > 0)
+                result[i] = 2;
+            else if (dnaPrefixNum[2][to] - dnaPrefixNum[2][from] > 0)
+                result[i] = 3;
+            else
+                result[i] = 4;
+        }
+
+        return result;
+
+    }
 }
