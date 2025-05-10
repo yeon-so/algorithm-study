4579e10d5d2ae4d35bab46f42dafc4123849352c
yeon-so <nordstromm03@gmail.com>
2025-05-10 20:55:33 +0900

    Level 2 - n진수 게임 다른 방식

--- programmers/2018-KAKAO-BLIND/n진수_게임.java
+++ programmers/2018-KAKAO-BLIND/n진수_게임.java
@@ -3,7 +3,7 @@ package programmers.2018-KAKAO-BLIND;
 public class n진수_게임 {
 
     // Integer.toString(num, 진수) 사용 
-    public String solution(int n, int t, int m, int p) {
+    public String solution1(int n, int t, int m, int p) {
         
         StringBuilder sb1 = new StringBuilder();
         int num = 0;
@@ -21,4 +21,44 @@ public class n진수_게임 {
         String answer = sb2.toString().toUpperCase();
         return answer;
     }
+
+        // 직접 진법 변환 사용 
+        public String solution2(int n, int t, int m, int p) {
+        
+            StringBuilder sb1 = new StringBuilder();
+            int num = 0;
+            
+            while(sb1.length() < t * m){
+                sb1.append(numberConversion(num++, n));
+            }
+            
+            StringBuilder sb2 = new StringBuilder();
+            
+            for(int i = 0; i < t; i++) {
+                sb2.append(sb1.charAt(i * m + (p - 1)));
+            }
+            
+            String answer = sb2.toString().toUpperCase();
+            return answer;
+        }
+
+        public String numberConversion(int num, int n){
+            if(num == 0) {
+                return "0";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while(num < 0) {
+                int temp = num % n;
+                if(temp < 10) {
+                    sb.append(temp);
+                } else {
+                    sb.append((char)(temp - 10 + 'A'));
+                }
+                num /= n;
+            }
+
+            return sb.reverse().toString();
+        }
 }
